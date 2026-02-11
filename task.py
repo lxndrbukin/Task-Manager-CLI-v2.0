9539f24482088f07ed38f40b0b22aea647a49d56
Alexander Bukin <lxndrbukin@proton.me>
2026-02-11 07:52:53 +0000

    Add from_dict class method

--- task.py
+++ task.py
@@ -1,15 +1,30 @@
 import uuid
 from datetime import datetime
+from enum import Enum
+
+class Status(Enum):
+    PENDING = "pending"
+    IN_PROGRESS = "in_progress"
+    COMPLETED = "completed"
 
 class Task():
-    def __init__(self, title, desc, priority, status):
-        self._id = str(uuid.uuid4())
+    def __init__(self,
+                 title,
+                 desc,
+                 priority,
+                 status,
+                 creation_date=None,
+                 _id=None
+                 ):
+        self._id = str(uuid.uuid4()) if _id is None else _id
         self._title = title
         self._desc = desc
         self._priority = priority
         self._status = status
-        self._creation_date = datetime.now().isoformat()
-
+        self._creation_date = datetime.now().isoformat() if creation_date is None else creation_date
+    @property
+    def id(self):
+        return self._id
     @property
     def title(self):
         return self._title
@@ -38,3 +53,14 @@ class Task():
             "status": self.status,
             "creation_date": self.creation_date
         }
+
+    @classmethod
+    def from_dict(cls, task_data):
+        return cls(
+            task_data["id"],
+            task_data["title"],
+            task_data["desc"],
+            task_data["priority"],
+            task_data["status"],
+            task_data["creation_date"]
+        )
