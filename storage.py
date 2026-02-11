5f80e0186bef86a133a81ec3caff18c086ebbcf7
Alexander Bukin <lxndrbukin@proton.me>
2026-02-11 18:31:04 +0000

    Implement storage handling

---
+++ storage.py
@@ -0,0 +1,16 @@
+import json
+from task import Task
+
+def load_tasks(filepath):
+    try:
+        with open(filepath, "r") as json_file:
+            tasks = json.load(json_file)
+            return [Task.from_dict(task) for task in tasks]
+    except FileNotFoundError:
+        print(f"File at {filepath} not found")
+        return []
+
+def save_tasks(filepath, tasks):
+    with open(filepath, "w") as json_file:
+        data = [Task.to_dict(task) for task in tasks]
+        json.dump(data, json_file, indent=4, ensure_ascii=False)
